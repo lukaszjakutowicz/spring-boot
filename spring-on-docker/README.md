596fa9a9ff0d07be8f6b8fb37c55c31d7e708d65
ellyot <lukasz.jakutowicz@gmail.com>
2025-01-09 19:52:54 +0100

    feature/spring-boot-on-docker to main (#2)
    * Configure dockerfile and docker compose file
    
    * Add readme file

---
+++ spring-on-docker/README.md
@@ -0,0 +1,16 @@
+# Spring boot on docker example
+
+## Build project
+
+To build project use maven command
+```shell
+mvn clean install 
+```
+
+## Run
+
+Run application on docker environment using docker compose command
+
+```shell
+docker compose up --build
+```
---
+++ spring-on-docker/src/main/java/com/lukaszjakutowicz/springondocker/SpringOnDockerApplication.java
@@ -0,0 +1,13 @@
+package com.lukaszjakutowicz.springondocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOnDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOnDockerApplication.class, args);
+    }
+
+}
